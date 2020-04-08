eff7071f55dc261e68e0109ae4becc69f10acd58
Devin Warrick <dev.warr@gmail.com>
2020-04-08 15:41:01 -0500

    code player with basic init and str methods

--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+
+    def __init__(self, name, current, inv=None):
+        self.name = name
+        self.current = current
+        self.inv = inv if inv else []
+
+    
+    def __str__(self):
+        return f"Name: {self.name}\nInventory: {self.inv}\n\n{self.current}"
