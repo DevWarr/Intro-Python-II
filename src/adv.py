e4c989ab51e1c17c684b4398580e78cba03685c6
Devin Warrick <dev.warr@gmail.com>
2020-04-09 00:11:18 -0500

    Allows player name input; adds inventory to rooms

--- src/adv.py
+++ src/adv.py
@@ -1,21 +1,32 @@
 from room import Room
 from player import Player
+from utils.colors import color
 import time
 import os
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", "Dim light filters in from the south. Dusty passages run north and east."),
-
-    'overlook': Room("Grand Overlook", "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."),
-
-    'narrow':   Room("Narrow Passage", "The narrow passage bends here from west to north. The smell of gold permeates the air."),
-
-    'treasure': Room("Treasure Chamber", "You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."),
+    'outside':  Room(
+        "Outside Cave Entrance",
+        "North of you, the cave mount beckons",
+        ["stick"]),
+    'foyer':    Room(
+        "Foyer",
+        "Dim light filters in from the south. Dusty passages run north and east.",
+        []),
+    'overlook': Room(
+        "Grand Overlook",
+        "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.",
+        ["staff"]),
+    'narrow':   Room(
+        "Narrow Passage", 
+        "The narrow passage bends here from west to north. The smell of gold permeates the air.",
+        []),
+    'treasure': Room(
+        "Treasure Chamber", 
+        "You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.",
+        ["treasure"]),
 }
 
 
@@ -35,7 +46,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player = Player("name", room['outside'])
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -47,17 +58,16 @@ player = Player("name", room['outside'])
 #
 # If the user enters "q", quit the game.
 
-
-def main():
+def main(player):
     loop = True
     while loop:
-        # Clear the terminal
+        # Print message to User
         os.system('cls||clear')
-        # print all the things
-        print("\nYou:")
+        print("You:")
         print(player)
-        print("\n[n] North   [s] South   [e] East   [w] West       [q] Quit")
-        user_in = input("").lower()
+        print(color(
+            "\n~W[n] ~eNorth   ~W[s] ~eSouth   ~W[e] ~eEast   ~W[w] ~eWest       ~W[q] ~eQuit"))
+        user_in = input("").lower().strip()
 
         if user_in == 'q':
             break
@@ -72,17 +82,29 @@ def main():
 
 
 if __name__ == "__main__":
+    player = None
     while True:
+        # Message to User
         os.system('cls||clear')
-        print("\nThis adventure game requires a terminal 80 characters wide and 11 lines tall.")
+        print(color(
+            "\n~WThis adventure game requires a terminal 80 characters wide and 13 lines tall."))
         print("[ ][ ][ ][ ][ ][ ]\n[ ][ ][ ][ ][ ][ ]\n[ ][ ][ ][ ][ ][ ]\n[ ][ ][ ][ ][ ][ ]\n[ ][ ][ ][ ][ ][ ]\n")
         print("If you can't see all of this text, please resize your terminal.")
-        print(
-            "Once you're good to go, type [news] to begin. Or, type [q] to quit.")
-        user_in = input("").lower()
+        print(color(
+            "Once you're good to go, ~Wtype a player name~e to begin. Or, type ~W[q]~e to quit."))
+        user_in = input("").strip()
 
-        if user_in == 'q':
-            exit()
-        elif user_in == 'news':
+        if len(user_in) == 0:
+            continue
+        elif len(user_in) > 15:
+            print(color("~RThat name is too long! Please have a name less than 15 characters."))
+            time.sleep(1.1)
+        elif user_in == 'q':
             break
-    main()
+        else: 
+            player = Player(user_in, room['outside'])
+            print(color("~BStarting Game . . ."))
+            time.sleep(1.7)
+            break
+    # If the player entered a name, start. If not, exit.
+    main(player) if player else exit()
