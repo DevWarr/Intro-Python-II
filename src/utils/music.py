4df9badc3e4225b9d4b256073977c17f71082cdb
Devin Warrick <dev.warr@gmail.com>
2020-04-10 03:32:15 -0500

    Adds more sound effects

--- src/utils/music.py
+++ src/utils/music.py
@@ -1,10 +1,10 @@
 from utils.playsound_copy import playsound
-from assets.assets import main_track, intro_track
+from assets.assets import *
 import time
 import threading
 
 
-class Music(threading.Thread):
+class Track(threading.Thread):
 
     def __init__(self, track):
         super().__init__()
@@ -18,6 +18,7 @@ class Music(threading.Thread):
 
     def run(self):
         self.__playing = True
+        stop = None
         while True:
             # While we're looping, loop the track
             sound_info = playsound(self.__play, False)
@@ -35,7 +36,9 @@ class Music(threading.Thread):
                     stop()
                     break
             if not self.__loop:
-                break
+                if stop:
+                    stop()
+                return
 
     def play_track(self, loop=True):
         self.__loop = loop
@@ -47,13 +50,14 @@ class Music(threading.Thread):
     def stop_track(self):
         self.__playing = False
 
-class MusicPlayer:
 
-    def __init__(self):
-        self.album = [main_track, intro_track]
+class Player:
+
+    def __init__(self, album):
+        self.album = album
         self.playing = False
         self.track = None
-    
+
     def play_track(self, num=0, loop=True):
         """
         Plays a track from the album.
@@ -66,7 +70,7 @@ class MusicPlayer:
 
         """
         if 0 <= num < len(self.album):
-            self.track = Music(self.album[num])
+            self.track = Track(self.album[num])
             self.track.play_track(loop)
 
     def stop_track(self):
@@ -75,3 +79,36 @@ class MusicPlayer:
             self.track.stop_track()
             self.track.join()
             self.track = None
+
+
+class MusicPlayer(Player):
+
+    def __init__(self):
+        super().__init__(music_files)
+
+    def play_track(self, num=0, loop=True):
+        """
+        Tracks:
+        -   0 -> main_track
+        -   1 -> intro_track
+        """
+        return super().play_track(num=num, loop=loop)
+
+
+class SoundPlayer(Player):
+
+    def __init__(self):
+        super().__init__(sound_files)
+
+    def play_track(self, num=0, loop=False):
+        """
+        Tracks:
+        -   0 -> damage
+        -   1 -> monster_death
+        -   2 -> correct answer
+        -   3 -> incorrect answer
+        -   4 -> monster spawn
+        -   5 -> menu sound
+        -   6 -> menu fail
+        """
+        return super().play_track(num=num, loop=loop)
