c0d52e3f50ad8ee76ffac7d42d6f01708a20d86f
Devin Warrick <dev.warr@gmail.com>
2020-04-11 13:52:28 -0500

    Updates test files

--- src/asset_test.py
+++ src/asset_test.py
@@ -1,12 +1,12 @@
 from assets.guardian_poses import *
 
 print("\n\nDIVID GUARIAN:\n")
-print(divid_guardian())
+print(divid_guardian()["incorrect"])
 print("\n\nMULTIP GUARDIAN \n")
-print(multip_guardian())
+print(multip_guardian()["incorrect"])
 print("\n\nSQUARE GUARDIAN\n")
-print(square_guardian())
+print(square_guardian()["incorrect"])
 print("\n\nRADICAL GUARDIAN\n")
-print(radical_guardian())
+print(radical_guardian()["incorrect"])
 print("\n\nARTIFACT GUARDIAN\n")
-print(artifact_guardian())
+print(artifact_guardian()["incorrect"])
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
+from random import randint, choice
+from threading import Thread
+import time
+
 class Test:
 
     def __init__(self):
         self.__list = [None, None, None, None]
         #              ↑n    ↑s    ↑e    ↑w
-
 
     # Property = the getter for an attribute
     @property
@@ -14,12 +17,11 @@ class Test:
     @property
     def n_to(self):
         return self.__list[0]
-        
+
     # Setter = the setter for an attribute
     @n_to.setter
     def n_to(self, name):
         self.__list[0] = name
-
 
     @property
     def s_to(self):
@@ -30,9 +32,62 @@ class Test:
         self.__list[1] = name
 
 
-
 # t = Test()
 # print(t.list) # → [None, None, None, None]
 # t.n_to = "new"
 # print(t.list) # → ["new", None, None, None]
 # print(t.n_to) # → "new"
+difficulty = 0
+count = 0
+squares = [4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225]
+cubes = [8, 27, 64, 125, 216, 343]
+
+num = 0
+is_square = True
+
+if difficulty == 0:
+    num = choice(squares[:10])
+    is_square = True
+elif difficulty == 1:
+    num = choice(squares[5:])
+    is_square = True
+else:
+    num = choice(cubes)
+    is_square = False
+
+# print(num, is_square)
+
+
+class Test:
+    def __init__(self):
+        self.__a = 1
+    @property
+    def a(self):
+        raise AttributeError
+    @a.setter
+    def a(self, val):
+        self.__a = val
+
+result = None
+def background_calculation():
+    # here goes some long calculation
+    time.sleep(randint(1, 5))
+
+    # when the calculation is done, the result is stored in a global variable
+    global result
+    result = 42
+
+def main():
+    thread = Thread(target=background_calculation)
+    thread.start()
+    print("waiting")
+
+    # wait here for the result to be available before continuing
+    thread.join()
+    background_calculation()
+    print("waiting")
+
+    print('The result is', result)
+
+if __name__ == '__main__':
+    main()
