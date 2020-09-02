931e9f12f6669c04e12b65391d004959054da862
Devin Warrick <dev.warr@gmail.com>
2020-09-01 21:57:04 -0500

    Adds script for pyinstaller

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,19 @@
 import PyInstaller.__main__
 import os
+import platform
 
 
 if __name__ == "__main__":
     FILEPATH = os.path.dirname(__file__)
+    separator = ";" if platform.system() == "Windows" else ":"
+
     PyInstaller.__main__.run([
         "--name=Adventure",
         "--onefile",
         "--windowed",
         "--distpath=../dist",
         "--workpath=../build/adv",
-        f"--add-data={os.path.join(FILEPATH, 'assets/WAV/')};assets/WAV",
-        f"--add-data={os.path.join(FILEPATH, 'assets/GIF/')};assets/GIF",
+        f"--add-data={os.path.join(FILEPATH, 'assets/WAV/')}{separator}assets/WAV",
+        f"--add-data={os.path.join(FILEPATH, 'assets/GIF/')}{separator}assets/GIF",
         "adv.py"
     ])
