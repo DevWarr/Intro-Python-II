585945ce06ba6e31431cbef6d92645080851d0c1
Devin Warrick <dev.warr@gmail.com>
2021-03-04 00:22:08 -0600

    Updates test format to match new style

--- JS/src/models/Item.test.js
+++ JS/src/models/Item.test.js
@@ -4,9 +4,8 @@ const { describe } = require('../utils/testFunction');
 const { Item } = require('./Item');
 
 describe('Item tests', [
-	[
-		'item creation has the proper name and description',
-		() => {
+	{
+		'item creation has the proper name and description': () => {
 			const itemName = 'Test Item';
 			const itemDescription = 'A wonderful item!';
 			const newItem = new Item('Test Item', 'A wonderful item!');
@@ -15,11 +14,10 @@ describe('Item tests', [
 			assert.equal(newItem.name, itemName);
 			assert.equal(newItem.description, itemDescription);
 		},
-	],
+	},
 
-	[
-		'item creation has no description if not given',
-		() => {
+	{
+		'item creation has no description if not given': () => {
 			const itemName = 'Test Item';
 			const newItem = new Item('Test Item');
 
@@ -27,5 +25,5 @@ describe('Item tests', [
 			assert.equal(newItem.name, itemName);
 			assert.equal(newItem.description, '');
 		},
-	],
+	},
 ]);
--- JS/src/models/Room.test.js
+++ JS/src/models/Room.test.js
@@ -4,9 +4,8 @@ const { describe } = require('../utils/testFunction');
 const { Room } = require('./Room');
 
 describe('Room tests', [
-	[
-		'Room creation has the proper name, description, and inventory',
-		() => {
+	{
+		'Room creation has the proper name, description, and inventory': () => {
 			const inventory = [1, 2, 3];
 			const testRoom = new Room('testRoom', 'testDescription', inventory);
 
@@ -14,31 +13,28 @@ describe('Room tests', [
 			assert.equal(testRoom.description, 'testDescription');
 			assert.equal(testRoom.inventory, inventory);
 		},
-	],
-	[
-		'Room successfully removes and returns item from inventory',
-		() => {
+	},
+	{
+		'Room successfully removes and returns item from inventory': () => {
 			const testRoom = new Room('testRoom', 'testDescription', [{ name: '1' }, { name: '4' }]);
 			const returnedItem = testRoom.remove_from_inventory('4');
 
 			assert.equal(testRoom.inventory.length, 1);
 			assert.equal(testRoom.inventory[0].name, '1');
-            assert.equal(returnedItem.name, '4')
+			assert.equal(returnedItem.name, '4');
 		},
-	],
-    [
-        'Room returns null and removes nothing if the item is not found in inventory',
-        () => {
-            const testRoom = new Room('testRoom', 'testDescription', [{ name: '1' }, { name: '4' }]);
+	},
+	{
+		'Room returns null and removes nothing if the item is not found in inventory': () => {
+			const testRoom = new Room('testRoom', 'testDescription', [{ name: '1' }, { name: '4' }]);
 			const returnedItem = testRoom.remove_from_inventory('17');
 
 			assert.equal(testRoom.inventory.length, 2);
-			assert.equal(returnedItem, null)
-        }
-    ],
-	[
-		'Room successfully adds an item to inventory',
-		() => {
+			assert.equal(returnedItem, null);
+		},
+	},
+	{
+		'Room successfully adds an item to inventory': () => {
 			const testRoom = new Room('testRoom', 'testDescription', [{ name: '1' }, { name: '4' }]);
 			testRoom.add_to_inventory({ name: '2' });
 			const expectedInventoryIndex = 2;
@@ -46,5 +42,5 @@ describe('Room tests', [
 			assert.equal(testRoom.inventory.length, 3);
 			assert.equal(testRoom.inventory[expectedInventoryIndex].name, '2');
 		},
-	],
+	},
 ]);
