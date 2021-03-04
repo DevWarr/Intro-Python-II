1658b9ed8eadb3e81d839a1f768a75f8353f49af
Devin Warrick <dev.warr@gmail.com>
2021-03-04 00:21:52 -0600

    Adds specific assertions if comparing to 'true'

--- JS/src/utils/testFunction.js
+++ JS/src/utils/testFunction.js
@@ -1,26 +1,82 @@
+const assert = require('assert').strict
 const { color } = require('./colors')
 
-const test = async (str, func) => {
+const test = async (testObj) => {
+    const testName = Object.keys(testObj)[0]
+    const testInfo = testObj[testName]
     try {
-        process.stdout.write(color(`~W[ ] ~e~y${str}~e`))
-        await func()
-        process.stdout.write(color(`\r~G[✓] ~e~y${str}~e\n`))
+        if (Array.isArray(testInfo)) await describe(testName, testInfo, true)
+        else {
+            process.stdout.write(color(`~W[ ] ~e~y${testName}~e`))
+            await testInfo()
+            process.stdout.write(color(`\r~G[✓] ~e~y${testName}~e\n`))
+        }
     } catch (err) {
-        process.stdout.write(color(`\r~R[X] ~e~p${str}~e\n`))
+        process.stdout.write(color(`\r~R[X] ~e~p${testName}~e\n`))
         const errorMessage = err.message
         console.error(`${errorMessage}\n`)
     }
 }
 
-const describe = async (str, testsArray) => {
-    console.log(str)
-    for (testInfo of testsArray) {
-        await test(...testInfo)
+const describe = async (str, testsArray, nested = false) => {
+    console.log(color(`${nested ? '' : '~W'}${str}`))
+    try {
+        for (testObj of testsArray) {
+            await test(testObj)
+        }
+    } catch (err) {
+        console.error(err)
     }
-    console.log()
+    if (!nested) console.log()
+}
+
+const assertLessThan = (value, compare) => {
+    try {
+        assert.equal(value < compare, true)
+    } catch (err) {
+        throw {message: `${value} is not less than ${compare}`}
+    }
+}
+
+const assertGreaterThan = (value, compare) => {
+    try {
+        assert.equal(value > compare, true)
+    } catch (err) {
+        throw {message: `${value} is not greater than ${compare}`}
+    }
+}
+
+const assertLessThanOrEqualTo = (value, compare) => {
+    try {
+        assert.equal(value <= compare, true)
+    } catch (err) {
+        throw {message: `${value} is not less than or equal to ${compare}`}
+    }
+}
+
+const assertGreaterThanOrEqualTo = (value, compare) => {
+    try {
+        assert.equal(value >= compare, true)
+    } catch (err) {
+        throw {message: `${value} is not greater than or equal to ${compare}`}
+    }
+}
+
+const assertIncludes = (array, value) => {
+    try {
+        assert.equal(array.includes(value), true);
+    } catch (err) {
+        if (array.length > 1000) array = 'the array (not printed because length exceeds 1000)'
+        throw{message: `${value} is not in ${array}`}
+    }
 }
 
 module.exports = {
     test,
-    describe
+    describe,
+    assertIncludes,
+    assertGreaterThan,
+    assertGreaterThanOrEqualTo,
+    assertLessThan,
+    assertLessThanOrEqualTo
 }
