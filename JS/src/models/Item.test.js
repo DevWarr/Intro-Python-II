f3dd8a911046c2ebd1ad3a7943e333e911e6ef50
Devin Warrick <dev.warr@gmail.com>
2021-03-07 20:56:05 -0600

    Cleans tests by removing extra describe() blocks

--- JS/src/models/Item.test.js
+++ JS/src/models/Item.test.js
@@ -2,23 +2,21 @@ const { test, describe, expect } = require('@jest/globals');
 
 const { Item } = require('./Item');
 
-describe('Item tests', () => {
-    test('item creation has the proper name and description', () => {
-        const itemName = 'Test Item';
-        const itemDescription = 'A wonderful item!';
-        const newItem = new Item('Test Item', 'A wonderful item!');
+test('item creation has the proper name and description', () => {
+    const itemName = 'Test Item';
+    const itemDescription = 'A wonderful item!';
+    const newItem = new Item('Test Item', 'A wonderful item!');
 
-        expect(newItem.toString()).toEqual(itemName);
-        expect(newItem.name).toEqual(itemName);
-        expect(newItem.description).toEqual(itemDescription);
-    });
+    expect(newItem.toString()).toEqual(itemName);
+    expect(newItem.name).toEqual(itemName);
+    expect(newItem.description).toEqual(itemDescription);
+});
 
-    test('item creation has no description if not given', () => {
-        const itemName = 'Test Item';
-        const newItem = new Item('Test Item');
+test('item creation has no description if not given', () => {
+    const itemName = 'Test Item';
+    const newItem = new Item('Test Item');
 
-        expect(newItem.toString()).toEqual(itemName);
-        expect(newItem.name).toEqual(itemName);
-        expect(newItem.description).toEqual('');
-    });
+    expect(newItem.toString()).toEqual(itemName);
+    expect(newItem.name).toEqual(itemName);
+    expect(newItem.description).toEqual('');
 });
--- JS/src/models/Room.test.js
+++ JS/src/models/Room.test.js
@@ -2,39 +2,37 @@ const { test, describe, expect } = require('@jest/globals');
 
 const { Room } = require('./Room');
 
-describe('Room tests', () => {
-    test('Room creation has the proper name, description, and inventory', () => {
-        const inventory = [1, 2, 3];
-        const testRoom = new Room('testRoom', 'testDescription', inventory);
+test('Room creation has the proper name, description, and inventory', () => {
+    const inventory = [1, 2, 3];
+    const testRoom = new Room('testRoom', 'testDescription', inventory);
 
-        expect(testRoom.name).toEqual('testRoom');
-        expect(testRoom.description).toEqual('testDescription');
-        expect(testRoom.inventory).toEqual(inventory);
-    });
+    expect(testRoom.name).toEqual('testRoom');
+    expect(testRoom.description).toEqual('testDescription');
+    expect(testRoom.inventory).toEqual(inventory);
+});
 
-    test('Room successfully removes and returns item from inventory', () => {
-        const testRoom = new Room('testRoom', 'testDescription', [{ name: '1' }, { name: '4' }]);
-        const returnedItem = testRoom.remove_from_inventory('4');
+test('Room successfully removes and returns item from inventory', () => {
+    const testRoom = new Room('testRoom', 'testDescription', [{ name: '1' }, { name: '4' }]);
+    const returnedItem = testRoom.remove_from_inventory('4');
 
-        expect(testRoom.inventory.length).toEqual(1);
-        expect(testRoom.inventory[0].name).toEqual('1');
-        expect(returnedItem.name).toEqual('4');
-    });
+    expect(testRoom.inventory.length).toEqual(1);
+    expect(testRoom.inventory[0].name).toEqual('1');
+    expect(returnedItem.name).toEqual('4');
+});
 
-    test('Room returns null and removes nothing if the item is not found in inventory', () => {
-        const testRoom = new Room('testRoom', 'testDescription', [{ name: '1' }, { name: '4' }]);
-        const returnedItem = testRoom.remove_from_inventory('17');
+test('Room returns null and removes nothing if the item is not found in inventory', () => {
+    const testRoom = new Room('testRoom', 'testDescription', [{ name: '1' }, { name: '4' }]);
+    const returnedItem = testRoom.remove_from_inventory('17');
 
-        expect(testRoom.inventory.length).toEqual(2);
-        expect(returnedItem).toEqual(null);
-    });
+    expect(testRoom.inventory.length).toEqual(2);
+    expect(returnedItem).toEqual(null);
+});
 
-    test('Room successfully adds an item to inventory', () => {
-        const testRoom = new Room('testRoom', 'testDescription', [{ name: '1' }, { name: '4' }]);
-        testRoom.add_to_inventory({ name: '2' });
-        const expectedInventoryIndex = 2;
+test('Room successfully adds an item to inventory', () => {
+    const testRoom = new Room('testRoom', 'testDescription', [{ name: '1' }, { name: '4' }]);
+    testRoom.add_to_inventory({ name: '2' });
+    const expectedInventoryIndex = 2;
 
-        expect(testRoom.inventory.length).toEqual(3);
-        expect(testRoom.inventory[expectedInventoryIndex].name).toEqual('2');
-    });
+    expect(testRoom.inventory.length).toEqual(3);
+    expect(testRoom.inventory[expectedInventoryIndex].name).toEqual('2');
 });
