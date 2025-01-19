cad3a072b2e13e9d4d03b102838df2ded1e64e82
Devin Warrick <dev.warr@gmail.com>
2025-01-19 13:47:28 -0600

    Use controls container and new font sizing for game
    Signed-off-by: Devin Warrick <dev.warr@gmail.com>

--- JS/src/main.ts
+++ JS/src/main.ts
@@ -1,15 +1,18 @@
 import { Application, Sprite, Text, Texture } from "pixi.js";
 import { GameContainer } from "./views/GameContainer";
-import { sleep } from "./utils/time";
+import { colorTest } from "./utils/colors";
+import { ControlsContainer, ControlType } from "./views/ControlsContainer";
+import { FONT_SIZE_PX } from "./models/SizeVector2";
+import { textSizeTest } from "./views/buildTextListWithColors";
 
-const ONE_REM_IN_PX = 16;
+const MAX_CHARACTERS_WIDTH = 85;
 
 const app = new Application();
 await app.init({
-  width: 82 * ONE_REM_IN_PX,
-  height: 20 * ONE_REM_IN_PX,
+  width: MAX_CHARACTERS_WIDTH * FONT_SIZE_PX.w,
+  height: 20 * FONT_SIZE_PX.h,
 });
-app.renderer.view.resolution = 1;
+app.renderer.view.resolution = 2;
 document.getElementById("app")!.appendChild(app.canvas);
 
 const setBackgroundColor = (color: number, gameContainer: GameContainer) => {
@@ -21,63 +24,63 @@ const setBackgroundColor = (color: number, gameContainer: GameContainer) => {
 };
 
 const mapContainer = new GameContainer({
-  width: 22 * ONE_REM_IN_PX,
-  height: 5 * ONE_REM_IN_PX,
+  width: 22 * FONT_SIZE_PX.w,
+  height: 5 * FONT_SIZE_PX.h,
   x: 0,
-  y: 1 * ONE_REM_IN_PX,
+  y: 1 * FONT_SIZE_PX.h,
 });
 setBackgroundColor(0xffffff, mapContainer);
 
 const legendContainer = new GameContainer({
-  width: 21 * ONE_REM_IN_PX,
-  height: 5 * ONE_REM_IN_PX,
-  x: 22 * ONE_REM_IN_PX,
-  y: 1 * ONE_REM_IN_PX,
+  width: 21 * FONT_SIZE_PX.w,
+  height: 5 * FONT_SIZE_PX.h,
+  x: 22 * FONT_SIZE_PX.w,
+  y: 1 * FONT_SIZE_PX.h,
 });
 setBackgroundColor(0xff0000, legendContainer);
 
 const inventoryContainer = new GameContainer({
-  width: 20 * ONE_REM_IN_PX,
-  height: 5 * ONE_REM_IN_PX,
-  x: 43 * ONE_REM_IN_PX,
-  y: 1 * ONE_REM_IN_PX,
+  width: 20 * FONT_SIZE_PX.w,
+  height: 5 * FONT_SIZE_PX.h,
+  x: 43 * FONT_SIZE_PX.w,
+  y: 1 * FONT_SIZE_PX.h,
 });
 setBackgroundColor(0x00ff00, inventoryContainer);
 
 const infoContainer = new GameContainer({
-  width: 82 * ONE_REM_IN_PX,
-  height: 3 * ONE_REM_IN_PX,
+  width: MAX_CHARACTERS_WIDTH * FONT_SIZE_PX.w,
+  height: 3 * FONT_SIZE_PX.h,
   x: 0,
-  y: 7 * ONE_REM_IN_PX,
+  y: 7 * FONT_SIZE_PX.h,
 });
 setBackgroundColor(0x0000ff, infoContainer);
 
-const actionContainer = new GameContainer({
-  width: 82 * ONE_REM_IN_PX,
-  height: 1 * ONE_REM_IN_PX,
+const controlsContainer = new ControlsContainer({
+  width: MAX_CHARACTERS_WIDTH * FONT_SIZE_PX.w,
+  height: 1 * FONT_SIZE_PX.h,
   x: 0,
-  y: 11 * ONE_REM_IN_PX,
+  y: 11 * FONT_SIZE_PX.h,
 });
-setBackgroundColor(0xffff00, actionContainer);
+controlsContainer.setActiveControl(ControlType.INTRO);
 
 const inputContainer = new GameContainer({
-  width: 82 * ONE_REM_IN_PX,
-  height: 1 * ONE_REM_IN_PX,
+  width: MAX_CHARACTERS_WIDTH * FONT_SIZE_PX.w,
+  height: 1 * FONT_SIZE_PX.h,
   x: 0,
-  y: 12 * ONE_REM_IN_PX,
+  y: 12 * FONT_SIZE_PX.h,
 });
 
 const responseContainer = new GameContainer({
-  width: 82 * ONE_REM_IN_PX,
-  height: 1 * ONE_REM_IN_PX,
+  width: MAX_CHARACTERS_WIDTH * FONT_SIZE_PX.w,
+  height: 1 * FONT_SIZE_PX.h,
   x: 0,
-  y: 13 * ONE_REM_IN_PX,
+  y: 13 * FONT_SIZE_PX.h,
 });
 
 const responseText = new Text({
   text: "",
   style: {
-    fontSize: 16,
+    fontSize: FONT_SIZE_PX.h,
     fontFamily: "monospace",
     fontWeight: "normal",
     fill: "gray",
@@ -91,7 +94,7 @@ app.stage.addChild(mapContainer.container);
 app.stage.addChild(legendContainer.container);
 app.stage.addChild(inventoryContainer.container);
 app.stage.addChild(infoContainer.container);
-app.stage.addChild(actionContainer.container);
+app.stage.addChild(controlsContainer.container);
 app.stage.addChild(inputContainer.container);
 app.stage.addChild(responseContainer.container);
 
@@ -99,7 +102,7 @@ const TEXT_PLACEHOLDER = ">>  ";
 const playerInputText = new Text({
   text: TEXT_PLACEHOLDER,
   style: {
-    fontSize: 16,
+    fontSize: FONT_SIZE_PX.h,
     fontFamily: "monospace",
     fontWeight: "normal",
     fill: "white",
