5d73aa1b472d640b5c102a3386b0ed359fa092e8
Devin Warrick <dev.warr@gmail.com>
2025-01-20 13:19:23 -0600

    Create player input container
    Signed-off-by: Devin Warrick <dev.warr@gmail.com>

---
+++ JS/src/views/PlayerInputContainer.ts
@@ -0,0 +1,19 @@
+import { Container, ContainerOptions } from "pixi.js";
+import { buildTextListWithColors } from "./buildTextListWithColors";
+
+const TYPE_HERE_LOCATION_STRING = "~W>>  ~e";
+
+export class PlayerInputContainer {
+  constructor(
+    containerOptions: ContainerOptions,
+    public container: Container = new Container(containerOptions),
+    private typeHereLocationString: string = TYPE_HERE_LOCATION_STRING,
+  ) {
+    this.renderPlayerInput("");
+  }
+
+  public renderPlayerInput(message: string) {
+    this.container.removeChildren();
+    buildTextListWithColors(this.typeHereLocationString + message).forEach((text) => this.container.addChild(text));
+  }
+}
