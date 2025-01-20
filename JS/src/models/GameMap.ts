7bb6b1b6161b762fa3be03aa26072c0af1cec49e
Devin Warrick <dev.warr@gmail.com>
2025-01-20 11:28:31 -0600

    Update Room to hvae RoomType enum
    Signed-off-by: Devin Warrick <dev.warr@gmail.com>

--- JS/src/models/GameMap.ts
+++ JS/src/models/GameMap.ts
@@ -1,4 +1,4 @@
-import { Room, Shrine } from "./Room";
+import { Room, RoomType, Shrine } from "./Room";
 import { Item } from "./Item";
 import {
   MultipGuardian,
@@ -8,6 +8,7 @@ import {
   ArtifactGuardian,
   Guardian,
 } from "./Guardians";
+import { PositionVector2 } from "./PositionVector2";
 
 // 0 -> Entrance
 // 1 -> Tunnel
@@ -27,6 +28,7 @@ const MAP_TEMPLATE: number[][] = [
 interface RoomTemplate {
   name: string;
   description: string;
+  roomType: RoomType;
   inventory?: Item[];
   guardian?: Guardian;
 }
@@ -35,35 +37,47 @@ const ROOM_TEMPLATES: RoomTemplate[] = [
   {
     name: "Entrance",
     description: "Return here with the mathematical artifact. Use it to escape this maze!",
+    roomType: RoomType.ENTRANCE,
   },
-  { name: "Tunnel", description: "Nothing extravagant. An Empty tunnel." },
+
+  {
+    name: "Tunnel",
+    description: "Nothing extravagant. An Empty tunnel.",
+    roomType: RoomType.TUNNEL,
+  },
+
   {
     name: "Multip Shrine",
     description: "The Shrine of the Multip sign, granting the power of multiplication.",
+    roomType: RoomType.SHRINE,
     inventory: [new Item("Multip")],
     guardian: new MultipGuardian(),
   },
   {
     name: "Divid Shrine",
     description: "The Shrine of the Divid sign, granting the power of division.",
+    roomType: RoomType.SHRINE,
     inventory: [new Item("Divid")],
     guardian: new DividGuardian(),
   },
   {
     name: "Square Shrine",
     description: "The Shrine of the Square sign, granting the power of squaring.",
+    roomType: RoomType.SHRINE,
     inventory: [new Item("Square")],
     guardian: new SquareGuardian(),
   },
   {
     name: "Radical Shrine",
     description: "The Shrine of the Radical sign, granting the power of square and cube rooting.",
+    roomType: RoomType.SHRINE,
     inventory: [new Item("Radical")],
     guardian: new RadicalGuardian(),
   },
   {
     name: "Artifact Shrine",
     description: "The Shrine of the Ancient Mathematical Artifact! Grab it, and get out of here!",
+    roomType: RoomType.ARTIFACT,
     inventory: [new Item("Artifact")],
     guardian: new ArtifactGuardian(),
   },
@@ -94,12 +108,12 @@ export class GameMap {
   map: (Room | null)[][];
   entrance: Room;
 
-  constructor(template: number[][] = MAP_TEMPLATE) {
+  constructor(template: number[][] = MAP_TEMPLATE, entraceRoomPosition: PositionVector2 = new PositionVector2(1, 4)) {
     this.map = this.createMap(template);
-    this.entrance = this.map[4][1]!;
+    this.entrance = this.map[entraceRoomPosition.y][entraceRoomPosition.x]!;
   }
 
-  createMap = (template: number[][]) => {
+  private createMap = (template: number[][]) => {
     return template.map((rowOfRooms: number[]) =>
       rowOfRooms.map((roomId: number) => {
         const roomInfo = ROOM_TEMPLATES[roomId];
@@ -110,7 +124,7 @@ export class GameMap {
         // Ergo, we must make a shrine instead of a room
         if (roomInfo.guardian) {
           return new Shrine(roomInfo.name, roomInfo.description, roomInfo.inventory, roomInfo.guardian);
-        } else return new Room(roomInfo.name, roomInfo.description, roomInfo.inventory);
+        } else return new Room(roomInfo.name, roomInfo.description, roomInfo.roomType, roomInfo.inventory);
       }),
     );
   };
--- JS/src/models/Room.ts
+++ JS/src/models/Room.ts
@@ -1,12 +1,42 @@
 import { Item } from "./Item";
 import { Guardian } from "./Guardians";
 
+export enum RoomType {
+  EMPTY = "   ",
+  TUNNEL = "[ ]",
+  ENTRANCE = "[X]",
+  SHRINE = "[S]",
+  ARTIFACT = "[A]",
+}
+
 export class Room {
   constructor(
-    public name: string,
-    public description: string,
-    public inventory: Item[] = [],
+    private __name: string,
+    private __description: string,
+    private __roomType: RoomType,
+    private __inventory: Item[] = [],
   ) {}
+
+  get name() {
+    return this.__name;
+  }
+
+  get description() {
+    return this.__description;
+  }
+
+  get roomType() {
+    return this.__roomType;
+  }
+
+  /**
+   * Returns a copy of the room's inventory.
+   *
+   * THIS IS NOT MEANT TO BE USED FOR ANY ROOM MODIFICATON. Only meant for rendering info.
+   */
+  get inventory() {
+    return [...this.__inventory];
+  }
 
   /**
    * If possible, removes item from room.
@@ -14,44 +44,36 @@ export class Room {
    */
   removeFromInventory(name: string): null | Item {
     let item = null;
-    const itemIndex = this.inventory.findIndex((item) => item.name.toLowerCase() === name.toLowerCase());
+    const itemIndex = this.__inventory.findIndex((item) => item.name.toLowerCase() === name.toLowerCase());
     if (itemIndex > -1) {
-      item = this.inventory[itemIndex];
-      this.inventory.splice(itemIndex, 1);
+      item = this.__inventory[itemIndex];
+      this.__inventory.splice(itemIndex, 1);
     }
     return item;
   }
 
   addToInventory(item: Item) {
-    this.inventory.push(item);
+    this.__inventory.push(item);
   }
 }
 
 export class Shrine extends Room {
   private __guardian: Guardian | null;
 
-  constructor(name: string, description: string, inventory: Item[] = [], guardian: Guardian | null = null) {
-    super(name, description, inventory);
+  constructor(name: string, description: string, inventory: Item[] = [], guardian: Guardian) {
+    super(name, description, RoomType.SHRINE, inventory);
     this.__guardian = guardian;
-    if (this.__guardian != null) {
-      this.__guardian.shrine = this;
-    }
+    guardian.shrine = this;
   }
 
   get guardian() {
     return this.__guardian;
-  }
-
-  set guardian(guardian) {
-    if (guardian) {
-      guardian.shrine = this;
-    }
-    this.__guardian = guardian;
   }
 }
 
 export const DEBUG_ROOM = new Room(
   "You're an explorer, you see!",
   "Search through the Mathematical Caves, find the Artifact, and make your escape!",
+  RoomType.TUNNEL,
   [new Item("Please resize"), new Item("So you can see"), new Item("All of"), new Item("This text")],
 );
